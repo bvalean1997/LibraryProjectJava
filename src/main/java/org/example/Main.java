2c69639291c8b6b118cc2dd6085904ecdb0005e2
bvalean1997 <valean.bianca@yahoo.com>
2024-08-06 11:22:25 +0300

    Finished assignment D.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,21 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void LibraryDemo(String[] args) {
+    public static void main(String[] args) {
         Book historyBook = new Book("0-061-96436", "Sapiens", "Yuval Noah Harari" );
         Book myFavoriteNovel = new Book("0-061-962136", "Anna Karenina", "Leo Tolstoy" );
 
         Magazine myFashionMagazine = new Magazine(7242, "Vogue","Anna Wintour");
         Magazine myGeographyMagazine = new Magazine(1124, "National Geographic","Gilbert Hovey");
 
-        // not finished yet
-        List<String> list = new ArrayList<String>();
-        list.add(String.valueOf(historyBook));
-        list.add(String.valueOf(myFavoriteNovel));
-        list.add(String.valueOf(myFashionMagazine));
-        list.add(String.valueOf(myGeographyMagazine));
+        List<LibraryItem> library = new ArrayList<LibraryItem>();
+        library.add(historyBook);
+        library.add(myFavoriteNovel);
+        library.add(myFashionMagazine);
+        library.add(myGeographyMagazine);
 
-
+        for (LibraryItem libraryItem : library) {
+            System.out.println(libraryItem.getDescription());
+        }
     }
 
 }
